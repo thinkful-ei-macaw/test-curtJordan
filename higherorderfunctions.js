ceea1226edcecc334f6080f9c07c373e12eb3cb7
Curtis Sessions <cjsesh2018@gmail.com>
2020-02-10 16:35:43 -0700

    step 3

--- higherorderfunctions.js
+++ higherorderfunctions.js
@@ -1,5 +1,6 @@
 function createGreeting() {
-    console.log("Hi my name is Curtis and I am 27yrs old");
+    return 'Hi my name is Curtis and I am 27yrs old';
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
