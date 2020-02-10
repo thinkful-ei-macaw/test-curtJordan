b232751e6d51b2ace8fcdb1f754c99cded54d34f
Curtis Sessions <cjsesh2018@gmail.com>
2020-02-10 16:42:46 -0700

    step 5

--- higherorderfunctions.js
+++ higherorderfunctions.js
@@ -1,6 +1,14 @@
+function getYearOfBirth(age) {
+    return 2020 - age;
+}
+
+
+
+
+
 function createGreeting(name, age) {
-    const yearOfBirth = 2020-age;
-    return "Hi my name is ${name} and I am ${age}yrs old. I was born in ${yearOfBirth}";
+    const yob = getYearOfBirth(age);
+    return "Hi my name is ${name} and I am ${age}yrs old. I was born in ${yob}";
 }
 
 const greeting1 = createGreeting();
