50259d999ecb67857f0a69c83e8b7e25f7518cb0
Jordan <jordanknox@Jordans-MacBook-Pro-3.local>
2020-02-10 15:34:32 -0800

    returned the greeting instead of logging it out

--- fdrills.js
+++ fdrills.js
@@ -2,8 +2,10 @@ let name = Jordan;
 
 console.log(`Hi, my name is ${name} and I'm old.1`)
 
-function createGreeting {
-    console.log('Hi, name is Jordan');
+function createGreeting(){
+    return 'Hi, name is Jordan';
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
+
