a90ae15ed77982c161df3bd54d32721ffa384258
Jordan <jordanknox@Jordans-MacBook-Pro-3.local>
2020-02-10 16:07:56 -0800

    catch block

--- fdrills.js
+++ fdrills.js
@@ -3,23 +3,43 @@ let name = Jordan;
 console.log(`Hi, my name is ${name} and I'm old.1`)
 
 function createGreeting(name, age){
-    const yearOfBirth = 16 - age;
-    return `Hi, I was born ${yearOfBirth}`;
+   if (name  === undefined || age  === undefined) {
+    throw new Error ('Not valid');
+   }
+
+   if (age < 0) {
+       throw new Error('Age cannot be negative')
+   }
+
+   if (typeof age !== 'number') {
+       throw new TypeError('Age must be a number');
+   }
 }
+
+try {
+    const greeting1 = createGreeting('Jordan', 29)
+    console.log(greeting1);
+} catch(e) {
+    console.error(e.message);
+}
+
 
 const greeting1 = createGreeting();
 console.log(greeting1);
 
 function getYearOfBirth (age) {
     if (age < 0) {
-        throw new Error("age cannot be nevative")
+        throw new Error("age cannot be negative")
     }
     return yearOfBirth;
 }
 
+
 function creatGreeting(name, age) {
     const yearOfBirth = 16 - age;
 }
+
+
 
 
 
