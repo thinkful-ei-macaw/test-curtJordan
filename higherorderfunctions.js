5508b5f37f19eeb340c70a6241fd95da135967f0
Curtis Sessions <cjsesh2018@gmail.com>
2020-02-10 17:17:12 -0700

    step 8

--- higherorderfunctions.js
+++ higherorderfunctions.js
@@ -1,23 +1,27 @@
-function yearOfBirth(age) {
-    if (age < 0) {
-        throw new Error 'Age cannot be negative';
-    }
+function getYearOfBirth(age) {
     return 2020 - age;
 }
 
-
-
-
-
 function createGreeting(name, age) {
-    const yob = yearOfBirth(age);
+    if (name === undefined || age === undefined) {
+        throw new Error('Arguments not valid.');
+      }
+
+    if (age < 0) {
+        throw new Error 'Age cannot be negative';
+    }
+
+    if (typeof age !== 'number') {
+        throw new TypeError('Age must be a number');
+      }
+    const yob = getYearOfBirth(age);
     return "Hi my name is ${name} and I am ${age}yrs old. I was born in ${yob}";
 }
 
 try {
     const greeting1 = createGreeting();
 } catch (err) {
-    console.log(err);
+    console.error(err.message);
 } finally {
 console.log(greeting1);
 }
