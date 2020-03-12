38c158079171bbba2f3756456131ceb6d4508b4f
Jordan <jordanknox@Jordans-MacBook-Pro-3.local>
2020-03-11 19:28:25 -0700

    finished f2 drills

--- for-each-filter-map.js
+++ for-each-filter-map.js
@@ -21,3 +21,17 @@ function move (arr) {
 }
 move([[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]]);
 
+// reduce *******
+function decode(message){
+  let reducer = (value, word) => {
+    let char = (word.length == 3) ? ' ' : word[word.length - 1].toUpperCase();
+    return value + char;
+  }
+
+  let words = message.split(' ');
+  let output = words.reduce(reducer, '');
+
+  console.log(output);
+}
+
+decode('noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest');
